d29cf39346f5e1a7306f700374750dc65664bb34
Fr3aky-b <wizkie62@gmail.com>
2025-02-04 11:09:13 +0300

    kkk

---
+++ introduction.py
@@ -0,0 +1,7 @@
+#PRINT is a method used to display a value or output
+
+
+print("kabi")
+print("emobilis")
+print(39)
+print(100*2)
---
+++ variable.py
@@ -0,0 +1,20 @@
+# A VARIABLE is a container used to store a value
+
+numbers =10
+firstname ="faith"
+lastname ="kariuki"
+language = "python"
+a =10
+b =15
+
+
+
+
+print(numbers)
+print(firstname)
+print(lastname)
+print(firstname,lastname)
+print(language, "is a web programming language")
+print( a +b)
+print("the sum of",a,"and",b,"is",a+b)
+# the sum of 10 and 15 is 25
