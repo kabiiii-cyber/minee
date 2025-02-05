65a16a13b61f171e8d2335bbf168bb13f963651c
Fr3aky-b <wizkie62@gmail.com>
2025-02-05 12:07:34 +0300

    second

---
+++ controlstatements.py
@@ -0,0 +1,28 @@
+temperature = 45
+
+if temperature > 25 :
+   print("it is too hot")
+else:
+   print("it is too cold")
+
+# A program that returns the smallest number
+first = 67
+second = 90
+third = 13
+
+
+if first < second and first < third :
+   print(first, "is the smallest number")
+elif  second< first and second <third:
+  print(second,"is the smallest number")
+else:
+  print(third, "is the smallest number")
+
+
+number =0
+if number == 0:
+  print(number,"isa nuetral number")
+  if number % 2 == 0:
+    print(number, "is an even number")
+  else:
+    print(number, "is an odd number")
