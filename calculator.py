38a3f3298781ee85b8106e8c180decc86f26a9a8
Fr3aky-b <wizkie62@gmail.com>
2025-02-07 13:06:49 +0300

    fff

--- calculator.py
+++ calculator.py
@@ -7,36 +7,27 @@
 
 
 def add(x,y):
-    return x+y
+  return x+y
+
 def subtract(x,y):
-    return x-y
-def multiply(x,y):
-   return x*y
+  return x-y
+
 def divide(x,y):
-   return x/y
+  return x/y
 
-def calculator():
-  print("Select operator:")
-  print(" 1.Addition")
-  print(" 2.Subtraction")
-  print(" 3.Multiplication")
-  print(" 4.Division")
+def multiply(x,y):
+  return x*y
 
-choice=input("Enter choice(1/2/3/4):")
-if choice in ['1','2','3','4']:
-  num1=float(input("Enter first number:"))
-  num2=float(input("Enter second number:"))
-
-if choice=='1':
-  print(f"The result is:{add(num1,num2)}")
-elif choice=='2':
-  print(f"The result is:{subtract(num1,num2)}")
-elif choice=='3':
-  print(f"The result is:{multiply(num1,num2)}")
-elif choice=='4':
-  print(f"The result is:{divide(num1,num2)}")
-else:
-  print("Invalid input. Please enter a valid choice")
-calculator()
+# man program
+print("select operator")
+print(" 1 add")
+print("2 subtract")
+print("3 multiply")
+print("4 divide")
 
 
+# input from user
+choice = input("chose(1/12/3/4): ")
+
+if choice in (1,2,3,4):
+  num1 =float
