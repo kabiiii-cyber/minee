1f91d899209e486c49d95f15aa5abf7abfadb364
Fr3aky-b <wizkie62@gmail.com>
2025-02-10 11:39:39 +0300

    ddd

--- calculator.py
+++ calculator.py
@@ -5,29 +5,55 @@
 #else wrong operator
 
 
-
 def add(x,y):
   return x+y
-
 def subtract(x,y):
   return x-y
-
-def divide(x,y):
-  return x/y
-
 def multiply(x,y):
   return x*y
+def divide(x,y):
+  return x/y
+def divide(x,y):
+  if y!=0:
+    return x/y
+  else:
+    return "Error!Division by zero"
+def calculator():
+  print("Select operator:")
+  print("1. Addition")
+  print("2. Subtraction")
+  print("3. Multiplication")
+  print("4. Division")
 
-# man program
-print("select operator")
-print(" 1 add")
-print("2 subtract")
-print("3 multiply")
-print("4 divide")
+choice=input("Enter choice(1/2/3/4):")
+if choice in ['1','2','3','4']:
+  num1=float(input("Enter first number:"))
+  num2=float(input("Enter second number:"))
+
+if choice=='1':
+  print(f"The result is:{add(num1,num2)}")
+elif choice=='2':
+  print(f"The result is:{subtract(num1,num2)}")
+elif choice=='3':
+  print(f"The result is:{multiply(num1,num2)}")
+elif choice=='4':
+  print(f"The result is:{divide(num1,num2)}")
+else:
+  print("Invalid input. Please enter a valid choice")
+calculator()
 
 
-# input from user
-choice = input("chose(1/12/3/4): ")
 
-if choice in (1,2,3,4):
-  num1 =float
+
+
+
+
+
+
+
+
+
+
+
+
+
--- primenumber.py
+++ primenumber.py
@@ -13,5 +13,20 @@ else:
 
 
 def is_prime(number):
-  if number <= 1:
-     re
+  if number < 2:
+      return False
+  else:
+      return True
+
+  for i in range(2, int (number **0.5)+1) :
+    if number % i == 0 :
+      return false
+    else:
+      return true
+  num = int(input("enter number:"))
+
+  if is_prime(num):
+    print(num,"prime number")
+  else:
+    print (num,"not prime number")
+
